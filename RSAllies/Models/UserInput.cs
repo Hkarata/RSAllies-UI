be9b415b81bc58a417f7652f272e68c6b549ffca
hei karata <herishine00@gmail.com>
2024-04-13 13:05:39 +0300

    Add new UserInput form and model with validation
    Updated `Index.cshtml` to include a new form for user input, with fields for first name, last name, phone, and email. The form is part of a two-column layout and fields are bound to the `Input` model. Updated `Index.cshtml.cs` to include a new `Input` property of type `UserInput?` for form data binding. Added a new `UserInput` class in `UserInput.cs` with properties for the form fields, each decorated with data annotation attributes for validation.

---
+++ RSAllies/Models/UserInput.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RSAllies.Models
+{
+    public class UserInput
+    {
+        [Required,StringLength(20)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required, StringLength(20)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, StringLength(10)]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
--- RSAllies/Pages/Index.cshtml.cs
+++ RSAllies/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RSAllies.Models;
 
 namespace RSAllies.Pages
 {
@@ -7,6 +8,8 @@ namespace RSAllies.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
+        public UserInput? Input { get; set;}
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
