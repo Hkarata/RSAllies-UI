48b2216a140a136aa958d639bf60b325e6ae60fe
hei karata <herishine00@gmail.com>
2024-04-26 17:37:53 +0300

    Added user profile card and updated data fetching
    A new user profile card has been added to Index.cshtml, displaying user's profile information in English and Swahili. The data is fetched from the new UserData property in the IndexModel class in Index.cshtml.cs. The OnGet method has been updated to an asynchronous OnGetAsync method, which retrieves user's session data, checks its validity, and redirects to the Login page if invalid. If valid, the data is deserialized into the UserDto object and assigned to UserData. Newtonsoft.Json namespace has been added for data deserialization.

--- RSAllies/Pages/Profile/Index.cshtml.cs
+++ RSAllies/Pages/Profile/Index.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RSAllies.Data.Contracts;
 
 namespace RSAllies.Pages.Profile
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public UserDto? UserData { get; set; }
+        public IActionResult OnGetAsync()
         {
+            var session = HttpContext.Session.GetString("UserSession");
+            if (string.IsNullOrEmpty(session)) return RedirectToPage("/Login");
+            UserData = JsonConvert.DeserializeObject<UserDto>(session!);
+            return Page();
         }
     }
 }
